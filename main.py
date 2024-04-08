a2ff1d85c0e6c4d5a4828cf52d3ec1d518ab1a3d
Muhammed AKAR <muhammedakardev@gmail.com>
2024-04-08 23:00:10 +0300

    Outlier ve kayıp değerler ile ilgili işlemler yapıldı

--- main.py
+++ main.py
@@ -14,45 +14,23 @@ pd.set_option('display.width', 5000)
 df = pd.read_csv('dataset/ecommerce.csv')
 dff = pd.read_csv('dataset/sellers.csv')
 
-df.head()
-
 df = pd.merge(df, dff, on='seller_id')
 
 df.drop(['customer_unique_id', 'customer_zip_code_prefix', 'payment_sequential', 'review_comment_title',
          'product_name_length',
-         'product_description_length', 'seller_zip_code_prefix'
+         'product_description_length', 'seller_zip_code_prefix', 'product_photos_qty'
          ], inplace=True, axis=1)
 
-df.isnull().sum().sum()
-
-df.dropna(subset=['product category', 'product_photos_qty', 'product_weight_g', 'product_length_cm'
-    , 'product_height_cm', 'product_width_cm'], inplace=True)
-
-out.for_check(df, df.columns)
-
-low_th, up_th = out.outlier_thresholds(df, column=['freight_value'], q1=0.25, q3=0.75)
-
-df[df['freight_value']>102.63].shape
-
-df.describe().T
-
-df.sort_values(by='payment_value', ascending=True).tail(30)
-
-df['order_id'].value_counts()
+df.dropna(subset=['product category', 'product_weight_g', 'product_length_cm'
+    , 'product_height_cm', 'product_width_cm', 'order_approved_at', 'order_delivered_carrier_date',
+                  'order_delivered_customer_date'], inplace=True)
 
 df = df.drop_duplicates(subset=['order_id'])
 
-df.isnull().sum()
+gra.plot_numerical_col(df, num_cols=['price', 'payment_value', 'freight_value'])
 
-df.shape
-
-df.head()
-
-gra.plot_numerical_col(df, num_cols=['price', 'payment_value','freight_value'])
-
-
-df = out.replace_all_outliers(df, ['price', 'payment_value','freight_value','product_weight_g','product_width_cm'])
-
-df.shape
+df = out.replace_all_outliers(df, ['price', 'payment_value', 'freight_value', 'product_weight_g', 'product_width_cm'])
 
 out.for_check(df, df.columns)
+
+df.groupby('product_photos_qty')['review_score'].mean().sort_values(ascending=False)
