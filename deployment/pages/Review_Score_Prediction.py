5bf3ad803c75a718ff5deee01ecc93ab37624857
Ümit Kara <umitdkara@gmail.com>
2024-04-28 16:37:33 +0300

    Update Review_Score_Prediction.py

--- deployment/pages/Review_Score_Prediction.py
+++ deployment/pages/Review_Score_Prediction.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib  # Add joblib library
+from datetime import datetime, timedelta
+
 
 # Load the pickled model
 model = joblib.load('deployment/log_reg.pkl')  # Replace 'log_reg.pkl' with your actual filename
@@ -36,20 +38,33 @@ with tab2:
         st.session_state.current_tab = "Süreç"
 
 with tab3:
-    approval_time = st.number_input('Onay Süresi (dk)', min_value=0)
-    customer_wait_time = st.number_input('Müşteri Bekleme Süresi (gün)', min_value=0)
-    delivery_time_diff = st.number_input('Teslimat Süresi Farkı (gün)', min_value=0)
-    days_to_delivery_actual = st.number_input('Gerçek Teslimat Gün Sayısı', min_value=0)
-    seller_review_score = st.number_input('Satıcı Değerlendirme Puanı', min_value=0.0, max_value=5.0, step=0.1)
+    default_purchase_date = datetime(2017, 1, 1)
+    order_purchase_timestamp = st.date_input('Satın Alınan Tarih', value=default_purchase_date)
+
+    # order_delivered_carrier_date için varsayılan başlangıç tarihi (order_purchase_timestamp'tan bir gün sonrası)
+    if order_purchase_timestamp:
+        default_carrier_date = order_purchase_timestamp + timedelta(days=1)
+    else:
+        default_carrier_date = default_purchase_date + timedelta(days=1)
+    order_delivered_carrier_date = st.date_input('Kargoya Verilen Tarih', value=default_carrier_date)
+
+    # order_delivered_timestamp için varsayılan başlangıç tarihi (order_delivered_carrier_date'ten bir gün sonrası)
+    if order_delivered_carrier_date:
+        default_delivery_date = order_delivered_carrier_date + timedelta(days=1)
+    else:
+        default_delivery_date = default_purchase_date + timedelta(days=1)
+    order_delivered_timestamp = st.date_input('Müşteriye Ulaşan Tarih', value=default_delivery_date)
+
+    seller_review_score = st.number_input('Satıcı Değerlendirme Puanı', min_value=0, max_value=5, step=1)
 
     # Define the prediction function
     def predict_review_score(price, freight_value, discount, payment_type, payment_installments,
-                             category, approval_time, customer_wait_time, delivery_time_diff,
-                             days_to_delivery_actual, seller_review_score):
+                             category, order_purchase_timestamp, order_delivered_carrier_date, order_delivered_timestamp,
+                             seller_review_score):
         # Prepare the input features
         features = [price, freight_value, discount, payment_type, payment_installments,
-                   category, approval_time, customer_wait_time, delivery_time_diff,
-                   days_to_delivery_actual, seller_review_score]
+                   category, order_purchase_timestamp, order_delivered_carrier_date, order_delivered_timestamp,
+                   seller_review_score]
 
         # Make the prediction using the loaded model
         prediction = model.predict([features])
@@ -61,8 +76,8 @@ with tab3:
     if st.button('Tahmin Et'):
         # Gather input features from all tabs
         input_features = [price, freight_value, discount, payment_type, payment_installments,
-                         category, approval_time, customer_wait_time, delivery_time_diff,
-                         days_to_delivery_actual, seller_review_score]
+                         category, order_purchase_timestamp, order_delivered_carrier_date, order_delivered_timestamp,
+                   seller_review_score]
 
         # Call the prediction function
         predicted_score = predict_review_score(*input_features)  # Unpack the list
