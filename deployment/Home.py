6ad14a189ff9b742d3a1b095175a81297778d9c7
Ümit Kara <umitdkara@gmail.com>
2024-05-01 03:37:38 +0300

    main.md eklendi.

--- deployment/Home.py
+++ deployment/Home.py
@@ -19,7 +19,7 @@ st.set_page_config(
     }
 )
 
-st.sidebar.image("deployment/assets/logo.png" , use_column_width=True)
+st.sidebar.image("deployment/assets/logo.png", use_column_width=True)
 
 html_code = """
 <div style="display: flex;">
@@ -58,52 +58,10 @@ st.subheader('Grup Marşımız')
 st.audio("deployment/assets/datasapiens.mp3", format="audio/mpeg")
 '---'
 
+with open("deployment/assets/main.md", "r", encoding="utf-8") as file:
+    markdown_text = file.read()
 
-st.subheader('Proje Hakkında')
-
-st.markdown('''
-**Giriş:**
-
-Bu, Olist Store'da yapılan siparişlerin Brezilya e-ticaret genel veri kümesidir. Veri kümesi, 2016'dan 2018'e kadar Brezilya'daki birden fazla pazar yerinde yapılan 100 bin siparişin bilgisine sahiptir. Özellikleri, bir siparişi birden fazla boyuttan görüntülemeye olanak tanır: sipariş durumu, fiyat, ödeme ve nakliye performansından müşteri konumuna, ürün özelliklerine ve son olarak müşteriler tarafından yazılan incelemelere kadar. Ayrıca Brezilya posta kodlarını enlem/boylam koordinatlarıyla ilişkilendiren bir coğrafi konum veri seti de yayınladık.
-Bu gerçek ticari veridir, anonimleştirilmiştir ve inceleme metnindeki şirketlere ve ortaklara yapılan referanslar Game of Thrones'un büyük evlerinin isimleriyle değiştirilmiştir.
-
-
-**Müşteri Memnuniyeti Modelleme:**
-
-Müşteri memnuniyeti modelleme, müşteri memnuniyetini etkileyen faktörleri belirleme ve bu faktörlere dayalı olarak müşteri memnuniyetini tahmin etme sürecidir. Bu modeller, pazarlama kampanyalarını optimize etmek, ürün ve hizmetleri geliştirmek ve müşteri deneyimini iyileştirmek için kullanılabilir.
-
-**Olist Veri Seti:**
-
-Olist veri seti, müşteri memnuniyeti modelleme için ideal bir kaynaktır. Veri seti, siparişler, ürünler, müşteriler ve değerlendirmeler gibi müşteri memnuniyetini etkileyebilecek birçok faktörü içerir.
-
-**Modelleme Yöntemleri:**
-
-Müşteri memnuniyetini modellemek için çeşitli yöntemler kullanılabilir. Yaygın olarak kullanılan yöntemlerden bazıları şunlardır:
-
-*   **Regresyon analizi:** Regresyon analizi, bağımlı bir değişken (örneğin, müşteri memnuniyeti puanı) ile bir veya birden fazla bağımsız değişken (örneğin, ürün fiyatı, teslimat süresi) arasındaki ilişkiyi modellemek için kullanılır.
-    
-*   **Sınıflandırma:** Sınıflandırma, müşterileri memnun veya memnuniyetsiz olarak sınıflandırmak için kullanılır.
-    
-*   **Makine öğrenmesi:** Makine öğrenmesi, müşteri memnuniyetini etkileyen karmaşık ilişkileri otomatik olarak öğrenmek için kullanılır.
-    
-
-**Modelleme Değerlendirme:**
-
-Müşteri memnuniyeti modelleri, farklı ölçütler kullanılarak değerlendirilmelidir. Yaygın olarak kullanılan ölçütlerden bazıları şunlardır:
-
-*   **Ortalama mutlak hata (MAE):** MAE, tahmin edilen değerler ile gerçek değerler arasındaki ortalama mutlak farkı ölçer.
-    
-*   **Ortalama karesel hata (MSE):** MSE, tahmin edilen değerler ile gerçek değerler arasındaki ortalama karesel farkı ölçer.
-    
-*   **Doğruluk:** Doğruluk, doğru bir şekilde tahmin edilen müşteri sayısının toplam müşteri sayısına oranını ölçer.
-    
-
-**Sonuç:**
-
-Olist veri seti, müşteri memnuniyeti modelleme için değerli bir kaynaktır. Bu veri seti, siparişler, ürünler, müşteriler ve değerlendirmeler gibi çeşitli bilgileri içerir. Bu bilgiler, müşteri memnuniyetini etkileyen faktörleri belirlemek ve tahmin modelleri oluşturmak için kullanılabilir.
-''')
-
-'---'
+st.markdown(markdown_text)
 
 st.subheader('Proje Dosyaları')
 
@@ -121,5 +79,4 @@ st.markdown('''
         </div>
         <div style="text-align: center;">GitHub</div>
     </a>
-''',unsafe_allow_html=True)
-
+''', unsafe_allow_html=True)
---
+++ deployment/assets/main.md
@@ -0,0 +1,35 @@
+# Proje Hakkında  
+  
+Bu proje, Olist Store'da yapılan siparişlerin Brezilya e-ticaret genel veri kümesidir. Veri kümesi, 2016'dan 2018'e kadar Brezilya'daki birden fazla pazar yerinde yapılan 100 bin siparişin bilgisine sahiptir. Özellikleri, bir siparişi birden fazla boyuttan görüntülemeye olanak tanır: sipariş durumu, fiyat, ödeme ve nakliye performansından müşteri konumuna, ürün özelliklerine ve son olarak müşteriler tarafından yazılan incelemelere kadar. Ayrıca Brezilya posta kodlarını enlem/boylam koordinatlarıyla ilişkilendiren bir coğrafi konum veri seti de yayınladık. Bu gerçek ticari veridir, anonimleştirilmiştir ve inceleme metnindeki şirketlere ve ortaklara yapılan referanslar Game of Thrones'un büyük evlerinin isimleriyle değiştirilmiştir.  
+  
+## 🙋Müşteri Memnuniyeti Modelleme:  
+  
+Müşteri memnuniyeti modelleme, müşteri memnuniyetini etkileyen faktörleri belirleme ve bu faktörlere dayalı olarak müşteri memnuniyetini tahmin etme sürecidir. Bu modeller, pazarlama kampanyalarını optimize etmek, ürün ve hizmetleri geliştirmek ve müşteri deneyimini iyileştirmek için kullanılabilir.  
+  
+### 📊Olist Veri Seti:  
+  
+Olist veri seti, müşteri memnuniyeti modelleme için ideal bir kaynaktır. Veri seti, siparişler, ürünler, müşteriler ve değerlendirmeler gibi müşteri memnuniyetini etkileyebilecek birçok faktörü içerir.  
+  
+## 🎢Modelleme Yöntemleri:  
+  
+Müşteri memnuniyetini modellemek için çeşitli yöntemler kullanılabilir. Yaygın olarak kullanılan yöntemlerden bazıları şunlardır:  
+  
+Regresyon analizi: Regresyon analizi, bağımlı bir değişken (örneğin, müşteri memnuniyeti puanı) ile bir veya birden fazla bağımsız değişken (örneğin, ürün fiyatı, teslimat süresi) arasındaki ilişkiyi modellemek için kullanılır.  
+  
+Sınıflandırma: Sınıflandırma, müşterileri memnun veya memnuniyetsiz olarak sınıflandırmak için kullanılır.  
+  
+Makine öğrenmesi: Makine öğrenmesi, müşteri memnuniyetini etkileyen karmaşık ilişkileri otomatik olarak öğrenmek için kullanılır.  
+  
+## 📑Modelleme Değerlendirme:  
+  
+Müşteri memnuniyeti modelleri, farklı ölçütler kullanılarak değerlendirilmelidir. Yaygın olarak kullanılan ölçütlerden bazıları şunlardır:  
+  
+Ortalama mutlak hata (MAE): MAE, tahmin edilen değerler ile gerçek değerler arasındaki ortalama mutlak farkı ölçer.  
+  
+Ortalama karesel hata (MSE): MSE, tahmin edilen değerler ile gerçek değerler arasındaki ortalama karesel farkı ölçer.  
+  
+Doğruluk: Doğruluk, doğru bir şekilde tahmin edilen müşteri sayısının toplam müşteri sayısına oranını ölçer.  
+  
+# 🔍Sonuç:  
+  
+Olist veri seti, müşteri memnuniyeti modelleme için değerli bir kaynaktır. Bu veri seti, siparişler, ürünler, müşteriler ve değerlendirmeler gibi çeşitli bilgileri içerir. Bu bilgiler, müşteri memnuniyetini etkileyen faktörleri belirlemek ve tahmin modelleri oluşturmak için kullanılabilir.
