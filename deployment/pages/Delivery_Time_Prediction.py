b50ff8c724aa6693d6d2996835ceba1610c8623b
Ümit Kara <umitdkara@gmail.com>
2024-05-04 16:11:54 +0300

    düzenlemeler yapıldı.

--- deployment/pages/Delivery_Time_Prediction.py
+++ deployment/pages/Delivery_Time_Prediction.py
@@ -18,19 +18,7 @@ tab1, tab2, tab3 = st.tabs(["Tab1", "Tab2", "Tab3"])
 
 
 with (tab1):
-    product_weight_g = st.number_input("Ürün Ağırlığı")
     payment_value = st.number_input("Toplam Fiyat")
-    distance_km = st.slider('Mesafe', min_value=1, max_value=8736, value=1)
-    cities_status = st.radio("Şehir Statü", ["Big", "Major", "Medium", "Small"])
-    if cities_status == "Big":
-        cities_status = 0
-    elif cities_status == "Major":
-        cities_status = 1
-    elif cities_status == "Medium":
-        cities_status = 2
-    else:
-        cities_status = 3
-
     quantity = st.number_input("Adet")
     time_box= datetime(2017, 1, 1)
     time = st.date_input("Satın Alınan Tarih", time_box)
@@ -38,48 +26,6 @@ with (tab1):
     month = time.month
     days = time.weekday()
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][days]
-
-    season = ""
-    if month < 3:
-        season = "q1"
-    elif month < 6:
-        season = "q2"
-    elif month < 9:
-        season = "q3"
-    else:
-        season = "q4"
-
-    if season == "q1":
-        season = [0,0,0]
-    elif season == "q2":
-        season = [1,0,0]
-    elif season == "q3":
-        season = [0,1,0]
-    else:
-        season = [0,0,1]
-
-
-    product_cm3 = st.number_input("Ürün cm3'ü")
-    prepare_time = st.number_input("Ürünün Hazırlanma Süresi" , step=1)
-    cargo_score = st.number_input("Kargo Skoru" , step=1)
-
-    special_day= ["Carnival", "Children", "Christmas", "New Year", "Valentine's Day", "Normal", "Black Friday"]
-    special_days= st.selectbox("Özel Günler" , special_day)
-    if special_day == "Carnival":
-        special_day = [1, 0, 0, 0, 0, 0]
-    elif special_day == "Children":
-        special_day = [0, 1, 0, 0, 0, 0]
-    elif special_day == "Christmas":
-        special_day = [0, 0, 1, 0, 0, 0]
-    elif special_day == "New Year":
-        special_day = [0, 0, 0, 1, 0, 0]
-    elif special_day == "Valentine's Day":
-        special_day = [0, 0, 0, 0, 1, 0]
-    elif special_day =="Normal":
-        special_day = [0, 0, 0, 0, 0, 1]
-    else:
-        special_day = [0, 0, 0, 0, 0, 0]
-
     weekday = days
     if weekday == "Monday":
         weekday = [1, 0, 0, 0, 0, 0]
@@ -95,26 +41,25 @@ with (tab1):
         weekday = [0, 0, 0, 0, 0, 1]
     else:
         weekday = [0, 0, 0, 0, 0, 0]
-
-    state= ["PR" , "RJ" , "RS"  , "SC" , "RP", "SP", "Other"]
-    seller_state=("Satıcı Eyaleti" , state)
-    if state == "PR":
-        state = [1, 0, 0, 0, 0, 0]
-    elif state == "RJ":
-        state = [0, 1, 0, 0, 0, 0]
-    elif state == "RS":
-        state = [0, 0, 1, 0, 0, 0]
-    elif state == "Other":
-        state = [0, 0, 0, 1, 0, 0]
-    elif state == "SC":
-        state = [0, 0, 0, 0, 1, 0]
-    elif state == "SP":
-        state = [0, 0, 0, 0, 0, 1]
+    special_day = ["Carnival", "Children", "Christmas", "New Year", "Valentine's Day", "Normal", "Black Friday"]
+    special_days = st.selectbox("Özel Günler", special_day)
+    if special_day == "Carnival":
+        special_day = [1, 0, 0, 0, 0, 0]
+    elif special_day == "Children":
+        special_day = [0, 1, 0, 0, 0, 0]
+    elif special_day == "Christmas":
+        special_day = [0, 0, 1, 0, 0, 0]
+    elif special_day == "New Year":
+        special_day = [0, 0, 0, 1, 0, 0]
+    elif special_day == "Valentine's Day":
+        special_day = [0, 0, 0, 0, 1, 0]
+    elif special_day == "Normal":
+        special_day = [0, 0, 0, 0, 0, 1]
     else:
-        state = [0, 0, 0, 0, 0, 0]
-
+        special_day = [0, 0, 0, 0, 0, 0]
+with (tab2):
     c_state = ["CE" , "DF" , "ES" , "GO" , "MG" , "PE" , "PR" ,"RJ" , "RS" , "SC" , "SP", "BA" "Other"]
-    customer_state = st.selectbox("Satıcı Eyaleti" , c_state)
+    customer_state = st.selectbox("Müşteri Eyaleti" , c_state)
     if c_state == "CE":
         c_state = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     elif c_state == "DF":
@@ -141,6 +86,56 @@ with (tab1):
         c_state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
     else:
         c_state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    cities_status = st.radio("Şehir Statü", ["Big", "Major", "Medium", "Small"])
+    if cities_status == "Big":
+        cities_status = 0
+    elif cities_status == "Major":
+        cities_status = 1
+    elif cities_status == "Medium":
+        cities_status = 2
+    else:
+        cities_status = 3
+    state = ["PR", "RJ", "RS", "SC", "RP", "SP", "Other"]
+    seller_state = ("Satıcı Eyaleti", state)
+    if state == "PR":
+        state = [1, 0, 0, 0, 0, 0]
+    elif state == "RJ":
+        state = [0, 1, 0, 0, 0, 0]
+    elif state == "RS":
+        state = [0, 0, 1, 0, 0, 0]
+    elif state == "Other":
+        state = [0, 0, 0, 1, 0, 0]
+    elif state == "SC":
+        state = [0, 0, 0, 0, 1, 0]
+    elif state == "SP":
+        state = [0, 0, 0, 0, 0, 1]
+    else:
+        state = [0, 0, 0, 0, 0, 0]
+    prepare_time = st.number_input("Ürünün Hazırlanma Süresi" , step=1)
+
+with (tab3):
+    product_weight_g = st.number_input("Ürün Ağırlığı")
+    product_cm3 = st.number_input("Ürün cm3'ü")
+    cargo_score = st.number_input("Kargo Skoru" , step=1)
+    distance_km = st.slider('Mesafe', min_value=1, max_value=8736, value=1)
+    season = ""
+    if month < 3:
+        season = "q1"
+    elif month < 6:
+        season = "q2"
+    elif month < 9:
+        season = "q3"
+    else:
+        season = "q4"
+
+    if season == "q1":
+        season = [0,0,0]
+    elif season == "q2":
+        season = [1,0,0]
+    elif season == "q3":
+        season = [0,1,0]
+    else:
+        season = [0,0,1]
 
     def predict_delivery_time(weight_g_p, payment_v_p, distance_p, cities_p, quantity_p, year_p, cm3_p,
                              month_p, prepare_p, cargo_p, season_p_0,season_p_1,season_p_2, special_p_0,special_p_1,special_p_2,special_p_3,special_p_4,special_p_5,
